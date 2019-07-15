6e89dfbaf0b524057512c536a408c0dd67814b21
Shamyl Zakariya <shamyl@gmail.com>
2019-07-14 20:25:11 -0700

    Basic wind particle behavior
    traces path of a wind particle from emitter to target volume, bouncing the particle and transferring impulse

--- Assets/Scripts/WindSource.cs
+++ Assets/Scripts/WindSource.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+
 
 public class WindSource : MonoBehaviour
 {
@@ -11,34 +14,153 @@ public class WindSource : MonoBehaviour
     [SerializeField] float windVelocity = 1;
     [SerializeField] float windParticleMass = 0.001f;
 
+    private List<WindParticle> _particles = new List<WindParticle>();
+    private Dictionary<int, List<Vector3>> _particlePaths = new Dictionary<int, List<Vector3>>();
+    private float _secondsUntilNextParticlePruning;
+    private const float _particlePruningPeriod = 1;
+    private Vector3 _particlePruningBoundsOrigin;
+    private float _particlePruningBoundsRadius2;
+
     void Start()
     {
-        Debug.LogFormat("sleepThreshold: {0}", Physics.sleepThreshold);
-    }
+        _secondsUntilNextParticlePruning = _particlePruningPeriod;
 
+        Bounds b = target.CalculateBounds();
+        b.size *= 2;
+        float radius = Mathf.Max(new float[] { b.size.x, b.size.y, b.size.z });
+        _particlePruningBoundsOrigin = b.center;
+        _particlePruningBoundsRadius2 = radius * radius;
+
+        Debug.LogFormat("[WindSource::Start] - pruning origin: {0} radius: {1}", b.center, radius);
+    }
 
     void Update()
     {
+        // make the "fan" face the chime - this is just for appearance
         if (target != null)
         {
             Bounds targetBounds = target.CalculateBounds();
             transform.LookAt(targetBounds.center);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             // for now send a single pulse to the center of the first bell
             ChimeBell bell = bells[0];
-            Bounds b = bell.transform.CalculateBounds();
+            Vector3 start = transform.position + Random.onUnitSphere * 0.05f;
+            Vector3 dir = ((bell.transform.position + Random.onUnitSphere * 0.05f) - transform.position).normalized;
+            EmitWindParticle(start, dir);
+        }
 
-            if (bell.Collider.Raycast(new Ray(transform.position, (b.center - transform.position).normalized), out var hitInfo, float.MaxValue)) {        
-                Vector3 dir = (hitInfo.point - transform.position).normalized;
-                Vector3 force = dir * windVelocity * windParticleMass;
-                bell.Rigidbody.AddForceAtPosition(force, hitInfo.point, ForceMode.Impulse);
-            }
-            else 
+        UpdateWindParticles();
+
+        _secondsUntilNextParticlePruning -= Time.deltaTime;
+        if (_secondsUntilNextParticlePruning <= 0)
+        {
+            _secondsUntilNextParticlePruning = _particlePruningPeriod;
+            PruneWindParticles();
+        }
+    }
+
+    void UpdateWindParticles()
+    {
+        for (int i = 0; i < _particles.Count; i++)
+        {
+            WindParticle particle = _particles[i];
+            if (!particle.alive) { continue; }
+
+            List<Vector3> path = _particlePaths[particle.id];
+
+            // raycast against bell the short ray
+            Vector3 position = particle.position;
+            Vector3 nextPosition = position + (particle.dir * particle.velocity * Time.deltaTime);
+            float distanceTraveled = Vector3.Distance(nextPosition, position);
+
+            if (!particle.hasEnteredTargetBounds)
             {
-                Debug.LogFormat("[WindSource] - raycast missed bell");
+                if ((particle.position - _particlePruningBoundsOrigin).sqrMagnitude <= _particlePruningBoundsRadius2)
+                {
+                    particle.hasEnteredTargetBounds = true;
+                }
+            }
+            else
+            {
+                if ((particle.position - _particlePruningBoundsOrigin).sqrMagnitude > _particlePruningBoundsRadius2)
+                {
+                    particle.alive = false;
+                    _particles[i] = particle;
+                    continue;
+                }
+            }
+
+            ChimeBell bell = bells[0];
+            if (bell.Collider.Raycast(new Ray(position, particle.dir), out var hitInfo, distanceTraveled))
+            {
+                // we have a collision - apply impulse and deflect and reduce power of the particle
+                // compute the reflection (this is the new particle direction) and the incidence
+                // incidence goeas from -1 to 1 where -1 means we hit square on, and 1 means we 
+                // perfectly grazed the surface imparting no energy
+                Vector3 reflection = Vector3.Reflect(particle.dir, hitInfo.normal);
+                float incidence = Vector3.Dot(particle.dir, reflection);
+
+                // remap the incidence such that a value of 1 means full energy transfer and 0 means none
+                float energyTransfer = 1 - ((incidence + 1f) / 2f);
+                Vector3 force = particle.dir * particle.velocity * particle.mass * energyTransfer;
+                bell.Rigidbody.AddForceAtPosition(force, hitInfo.point, ForceMode.Impulse);
+
+                // reduce particle velocity
+                particle.velocity *= 1 - energyTransfer;
+                particle.dir = reflection;
+                particle.alive = particle.velocity > 1e-5f;
+
+                // update the particle position along the path up to collision and then the 
+                // remaining distance along the reflection
+                nextPosition = hitInfo.point + (reflection * Mathf.Max(distanceTraveled - hitInfo.distance, 0.01f));
+            }
+
+            // update the particle
+            particle.position = nextPosition;
+            path.Add(nextPosition);
+            _particles[i] = particle;
+
+            // now draw this particle's path
+            Vector3 a = path[0];
+            for (int j = 1, N = path.Count; j < N; j++)
+            {
+                Vector3 b = path[j];
+                Debug.DrawLine(a, b, particle.color);
+                a = b;
             }
         }
+    }
+
+    void PruneWindParticles()
+    {
+        foreach(WindParticle p in _particles) {
+            if (!p.alive) {
+                _particlePaths.Remove(p.id);
+            }
+        }
+        _particles = _particles.Where((p) => { return p.alive; }).ToList();
+    }
+
+    private int _particleId = 0;
+
+    void EmitWindParticle(Vector3 startPosition, Vector3 startDirection)
+    {
+        WindParticle p = new WindParticle()
+        {
+            id = _particleId++,
+            position = startPosition,
+            dir = startDirection,
+            velocity = windVelocity,
+            mass = windParticleMass,
+            alive = true,
+            color = Random.ColorHSV(0, 1, 0.7f, 0.9f, 0.9f, 1f),
+            hasEnteredTargetBounds = false,
+        };
+
+        _particlePaths.Add(p.id, (new Vector3[] { startPosition }).ToList());
+        _particles.Add(p);
     }
 }
