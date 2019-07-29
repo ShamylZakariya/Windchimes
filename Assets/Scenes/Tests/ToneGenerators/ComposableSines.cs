7f912426248bfc586f4b0959680e0b78d8cfd7f2
Shamyl Zakariya <shamyl@gmail.com>
2019-07-29 08:57:11 -0700

    POC for composable tone generator

---
+++ Assets/Scenes/Tests/ToneGenerators/ComposableSines.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ComposableSines : MonoBehaviour
+{
+    [Serializable]
+    public struct SineWaveSource
+    {
+        public float frequency;
+        [Range(0, 1)]
+        public float gain;
+        [Range(0, Mathf.PI * 2)]
+        public float phaseOffset;
+        public AnimationCurve envelope;
+        public bool loop;
+
+        //
+        //  Private statue
+        //
+
+        internal float phase;
+        internal double startTime;
+        internal double endTime;
+    }
+
+    [SerializeField]
+    SineWaveSource[] sines = null;
+
+    [SerializeField]
+    float frequencyMultiplier = 1f;
+
+    //
+    //  Private statue
+    //
+
+    private AudioSource _source;
+    private float _sampleRate = 0;
+    const float TWO_PI = Mathf.PI * 2;
+    bool _toneCompleted;
+
+
+    void Start()
+    {
+        _sampleRate = AudioSettings.outputSampleRate;
+        _source = GetComponent<AudioSource>();
+        _source.playOnAwake = false;
+        _source.Stop();
+    }
+
+    void Update()
+    {
+        if (_toneCompleted && _source.isPlaying)
+        {
+            _source.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!_source.isPlaying)
+            {
+                PlayTone();
+            }
+            else
+            {
+                _source.Stop();
+            }
+        }
+    }
+
+    void PlayTone()
+    {
+        for (int i = 0; i < sines.Length; i++)
+        {
+            SineWaveSource swc = sines[i];
+            swc.phase = 0;
+            swc.startTime = AudioSettings.dspTime;
+            swc.phaseOffset = i * TWO_PI / sines.Length;
+
+            float duration = swc.envelope[swc.envelope.length - 1].time;
+            swc.endTime = swc.startTime + (double)duration * 1.5; // some fudge
+
+            sines[i] = swc;
+        }
+        _source.Play();
+        _toneCompleted = false;
+    }
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        double now = AudioSettings.dspTime;
+        double dspTimeIncrement = 1.0 / (double)_sampleRate;
+        bool didGenerateAudio = false;
+
+        for (int s = 0, sEnd = sines.Length; s < sEnd; s++)
+        {
+            SineWaveSource swc = sines[s];
+            if (!swc.loop && now >= swc.endTime) continue;
+
+            float increment = frequencyMultiplier * swc.frequency * 2f * Mathf.PI / _sampleRate;
+
+            double age = AudioSettings.dspTime - swc.startTime;
+
+            for (int i = 0, iEnd = data.Length; i < iEnd; i += channels)
+            {
+                swc.phase = swc.phase + increment;
+                if (swc.phase > TWO_PI) { swc.phase = 0; }
+
+                float envelope = swc.envelope.Evaluate((float)age);
+                float value = (envelope * swc.gain * Mathf.Sin(swc.phase + swc.phaseOffset));
+
+                data[i] += value;
+
+                if (channels == 2)
+                {
+                    data[i + 1] += value;
+                }
+
+                age += dspTimeIncrement;
+            }
+
+            sines[s] = swc;
+            didGenerateAudio = true;
+        }
+
+        if (!didGenerateAudio)
+        {
+            Debug.LogFormat("[OnAudioFilterRead] - done generating tones; stopping audio source...");
+            _toneCompleted = true;
+        }
+    }
+}
--- NOTES.md
+++ NOTES.md
@@ -14,5 +14,18 @@
 - Trigger bell chime at runtime with intensity, duration, and an appropriate envelope based on the impact kinetics.
     - do we need to generate different tones based on the location of the impact on the bell? Or is the bell tone independent of impact site?
 
+### On Bell Tone Synthesis
+https://www.soundonsound.com/techniques/synthesizing-bells
+- Figure 8 is particularly informative.
+- Sine waves are fine.
+- We need a periodic warble (is this gain or frequency shifting?), to create the boing-oing-oing-oing quality
+    - "This means that bells produce two partials of almost (but not quite) identical frequencies, and these interfere (or 'beat') in the same way as do two synth oscillators of similar frequency."
+
+#### What needs to be built:
+At the very minimum, we need to be able to compose pairs of sine waves and envelopes. We need to be able to say, here's a sine wave at x freq, with an envelope of such and such. and then fire off a stack of these to be rendered in real time.
+    - so far this works but needs a little more:
+        - a phase offset per-channel
+
+
 ## Phase 3
 - Use Job System to run these rays in parallel
